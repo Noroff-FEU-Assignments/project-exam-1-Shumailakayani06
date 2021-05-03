8b0bc2c2444fe4a38fa89e86d176bbe6231ad611
Shumaila Kayani <shumailakayani059@gmail.com>
2021-05-03 10:05:16 +0200

    posts

--- blogpost.js
+++ blogpost.js
@@ -7,7 +7,7 @@ const id = new URLSearchParams (qString).get("id");
 
 if (!id) {window.location ="posts.html";}
 
-let pUrl = "https://flowerpowerlcb.com/wp-json/wp/v2/posts/"+ id;
+let pUrl = "https://flowerpowerlcb.com/wp-json/wp/v2/posts/"+ id + "?_embed";
 fetch(pUrl)
 .then (response => response.json())
 .then (data => {
@@ -28,16 +28,12 @@ function specificPost(data){
     <div>
    
     <h1>${data.title.rendered}</h1>
-    ${data.excerpt.rendered}
-    ${data._links["wp:featuredmedia"][0].href}
-   
-    
-    <a href="posts.html">back to posts</a>
-   
     </div>
     <div>
-    <img src="${data._links["wp:featuredmedia"][0].href}">
-    </div>  
+    <img src="${data._embedded["wp:featuredmedia"][0].source_url}">
+    </div> 
+    <p>${data.excerpt.rendered}</p>
+    <a href="posts.html">back to posts</a> 
     `
 
     blogPost.innerHTML = postContent;
