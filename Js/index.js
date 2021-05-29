830bded5dfdcc8f104e3db6d7b8150bebbb651dc
Shumaila Kayani <shumailakayani059@gmail.com>
2021-05-29 17:44:15 +0200

    carousel mobile

--- Js/index.js
+++ Js/index.js
@@ -4,6 +4,10 @@ let url = "https://flowerpowerlcb.com/wp-json/wp/v2/posts?_embed&per_page=12";
 const carouselImages = document.querySelector (".carousel__images");
 const carouselButtons = document.querySelectorAll(".carousel__button");
 const body = document.getElementsByTagName("body")
+const carouselImagesMob = document.querySelector(".carousel__images--mob")
+const carouselImagesIpad = document.querySelector(".carousel__images--ipad")
+const carouselButtonMob = document.querySelector(".carousel__buttonmob")
+const carouselButtonIpad= document.querySelector(".carousel__buttonipad")
 
 
 /* loader */
@@ -33,7 +37,8 @@ fetch(url)
 });
 
 console.log(apiImages);
-
+let carouselMob = [];
+let carouselIpad = [];
 function homePosts(data){
 let homeBlogPosts =[];
 for(let element in data){
@@ -48,10 +53,24 @@ for(let element in data){
     
     `
     ;
+
+    carouselMob +=`
+    <a href="blogpost.html?id=${data[element].id}">
+    <img class="carousel_image" src="${data[element]._embedded["wp:featuredmedia"][0].source_url}" alt="blog post images">
+    </a>
+    `;
+
+    carouselIpad += ` <a href="blogpost.html?id=${data[element].id}">
+    <img class="carousel_image" src="${data[element]._embedded["wp:featuredmedia"][0].source_url}" alt="blog post images">
+    </a> `
+ 
+
     console.log(data[element]);
     console.log(apiImages.length);
 }
 carouselImages.innerHTML= homeBlogPosts;
+carouselImagesMob.innerHTML =carouselMob;
+carouselImagesIpad.innerHTML= carouselIpad;
 
 
 
@@ -63,26 +82,85 @@ let translateX = 0;
 
 
 
-carouselButtons.forEach(buttons =>{
-buttons.addEventListener("click", event => {
-    
-   if(event.target.id === "previouss" ){
-       if(imageIndex !== 1){
-           imageIndex--;
-           translateX += 1080;
-       }
-   } else{
-       if (imageIndex !== numberOfImages) {
-           imageIndex++;
-           translateX -= 1080;
-       }
-   }
-   carouselImages.style.transform = `translateX(${translateX}px)`;
-});
-})
-;
 
-}
+
+
+
+
+carouselButtons.forEach(buttons =>{
+    buttons.addEventListener("click", event => {
+        
+       if(event.target.id === "previouss" ){
+           if(imageIndex !== 1){
+               imageIndex--;
+               translateX += 1080;
+           }
+       } else{
+           if (imageIndex !== numberOfImages) {
+               imageIndex++;
+               translateX -= 1080;
+           }
+       }
+       carouselImages.style.transform = `translateX(${translateX}px)`;
+    });
+    })
+    ;
+    
+    }
+
+
+
+
+    /* carousel mob */
+carouselButtonMob.forEach(buttons =>{
+    buttons.addEventListener("click", event => {
+        
+       if(event.target.id === "previoussmob" ){
+           if(imageIndex !== 1){
+               imageIndex--;
+               translateX += 1080;
+           }
+       } else{
+           if (imageIndex !== carouselImagesMob ) {
+               imageIndex++;
+               translateX -= 1080;
+           }
+       }
+       carouselImagesMob.style.transform = `translateX(${translateX}px)`;
+    });
+    })
+    ; 
+
+
+
+
+
+
+/* carousel Ipad */
+
+
+
+
+carouselButtonIpad.forEach(buttons =>{
+    buttons.addEventListener("click", event => {
+        
+       if(event.target.id === "previoussipad" ){
+           if(imageIndex !== 1){
+               imageIndex--;
+               translateX += 1080;
+           }
+       } else{
+           if (imageIndex !== carouselImagesIpad ) {
+               imageIndex++;
+               translateX -= 1080;
+           }
+       }
+       carouselImagesIpad.style.transform = `translateX(${translateX}px)`;
+    });
+    })
+    ;
+
+
 
 
     
@@ -141,7 +219,7 @@ navSlide();
 
 
 
-/* touch slide */
+
 
 
 
